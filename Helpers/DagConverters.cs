4aa02d8786739a1ca4ef4866894322bc36575a06
Haines Chan <hainescx@gmail.com>
2026-05-18 07:53:21 +0800

    fix: implement and apply InverseBooleanConverter to ComboBox IsEnabled

--- Helpers/DagConverters.cs
+++ Helpers/DagConverters.cs
@@ -43,3 +43,16 @@ public class BoolToMappingTextConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return !(value is bool b && b);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return !(value is bool b && b);
+    }
+}
